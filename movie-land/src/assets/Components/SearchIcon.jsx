61afd4f7caef9a7197cd5b1f701b100eae3f4ba9
SinhleM <sinhle260@gmail.com>
2025-06-26 21:41:29 +0200

    Commit your changes before pulling

--- movie-land/src/assets/Components/SearchIcon.jsx
+++ movie-land/src/assets/Components/SearchIcon.jsx
@@ -9,7 +9,7 @@ const SearchIcon = () => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 19.9 19.7"
       >
-        <title id="title">Search Icon</title>
+        <title id="title">Search change Icon</title>
         <desc id="desc">A magnifying glass icon.</desc>
         <g className="search-path" fill="none" stroke="#848F91">
           <path strokeLinecap="square" d="M18.5 18.3l-5.4-5.4" />
